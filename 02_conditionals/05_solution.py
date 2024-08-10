04218c301f9c2f4dca0a1f3cbc08fa556a16858e
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2024-08-10 21:09:36 +0530

    feat: Add weather activity suggestion program

---
+++ 02_conditionals/05_solution.py
@@ -0,0 +1,12 @@
+# Weather Activity Suggestion
+
+weather = input("Enter the weather condition: ").lower()
+
+if weather == 'sunny':
+    print("Go for a walk")
+elif weather == 'rainy':
+    print("Read a book")
+elif weather == 'snowy':
+    print("Build a snowman")
+else: 
+    print("Enter a valid condition.")
