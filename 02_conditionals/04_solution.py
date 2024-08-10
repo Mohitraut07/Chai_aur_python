e2dbb6d03dd407b8911cbccaa1cba6672be26fb0
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2024-08-10 21:05:50 +0530

    feat: Add condition for invalid fruit color in Fruit Ripeness Checker program

--- 02_conditionals/04_solution.py
+++ 02_conditionals/04_solution.py
@@ -1,16 +1,22 @@
 # Fruit Ripeness Checker
 
-
+fruit = input("Enter your fruit: ").lower()
 color = input("Enter your fruit color: ").lower()
 
-if color == 'green':
-    condition = 'unripe'
-elif color == 'yellow':
-    condition = 'ripe'
-elif color == 'brown':
-    condition = 'overripe'
+
+if fruit == 'banana':
+    if color == 'green':
+        condition = 'unripe'
+    elif color == 'yellow':
+        condition = 'ripe'
+    elif color == 'brown':
+        condition = 'overripe'
+    else: 
+        print("Enter a valid color")
+        exit()
 else: 
-    print("Enter a valid color")
+    print("Enter a valid fruit")
     exit()
+
 
 print("The Fruit is",condition)
