eb1683d9ba79dd79fa0c89e0f50a2d271282d1a1
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2024-08-11 14:27:07 +0530

    feat: Add input validation for number between 1 and 10

---
+++ 03_loops/07_solution.py
@@ -0,0 +1,8 @@
+# Validate input
+
+while True: 
+    num = int(input("Enter a number between 1 and 10: "))
+    if 1<= num <=10:
+        print("Thanks")
+    else:
+        print("Enter a valid number, try again!")
